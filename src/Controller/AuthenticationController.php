cf5117e9d3682813099c385024b60f1cffcbf9c0
Fabien Julio <fabienjulio5@gmail.com>
2021-09-29 20:22:39 +0300

    Creation inscription

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,7 +27,20 @@ class AuthenticationController extends AbstractController
     /**
      * @Route("/logout", name="logout-page")
      */
-    public function logout(): 
+    public function logout()
     {
+    }
+    /**
+     * @Route("/register", name="register-page")
+     */
+    public function register(): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        return $this->render('authentication/register.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+    /**
+     * 
+     */
+    private function getConfiguration($label, $placeholder, $options = []): array
+    {
+        return array_merge([
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ], $options);
+    }
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, $this->getConfiguration('Nom', 'Votre nom'))
+            ->add('prenom', TextType::class, $this->getConfiguration('Prenom', 'Votre prénom'))
+            ->add('email', EmailType::class, $this->getConfiguration('Email', 'foo@exemple.com'))
+            ->add('telephone', TextType::class, $this->getConfiguration('Téléphone', 'Votre numéro de téléphone'))
+            ->add('region')
+            ->add('password')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
